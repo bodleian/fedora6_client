9cbeffbb495d5e8f39594dde4591e39a5ecd1cc5
Tom Wrobel <thomas.wrobel@bodleian.ox.ac.uk>
2023-04-29 09:23:10 +0100

    Initial commit

---
+++ lib/fedora6/client.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "client/version"
+
+module Fedora6
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/fedora6/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fedora6::Client do
+  it "has a version number" do
+    expect(Fedora6::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
