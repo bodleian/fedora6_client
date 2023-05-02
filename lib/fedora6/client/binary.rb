5633dc2fd8754373c00f27b837257ecca7cf698a
Tom Wrobel <thomas.wrobel@bodleian.ox.ac.uk>
2023-05-02 15:48:31 +0100

    System is broken following rubocop. Thanks rubocop

--- lib/fedora6/client/binary.rb
+++ lib/fedora6/client/binary.rb
@@ -1,116 +1,114 @@
 # frozen_string_literal: true
 
 module Fedora6
-  module Client
-    # Fedora6::Client::Binary
-    # Class for interacting with Binary (Non-RDF binary resources)
-    class Binary < Client
-      attr_reader :config, :parent_uri, :binary_identifier, :binary_uri
+  # Fedora6::Client::Binary
+  # Class for interacting with Binary (Non-RDF binary resources)
+  class Client::Binary < Client
+    attr_reader :config, :parent_uri, :binary_identifier, :binary_uri
 
-      def initialize(config = nil, parent_uri = nil, binary_identifier = nil, binary_uri = nil)
-        super
-        @parent_uri = parent_uri
-        @binary_identifier = binary_identifier
-        @binary_uri = if binary_uri
-                        binary_uri
-                      elsif parent_uri && binary_identifier
-                        "#{parent_uri}/#{binary_identifier}"
-                      end
+    def initialize(config = nil, parent_uri = nil, binary_identifier = nil, binary_uri = nil)
+      super
+      @parent_uri = parent_uri
+      @binary_identifier = binary_identifier
+      @binary_uri = if binary_uri
+                      binary_uri
+                    elsif parent_uri && binary_identifier
+                      "#{parent_uri}/#{binary_identifier}"
+                    end
+    end
+
+    def save(binary_data, filename, transaction_uri: nil)
+      if exists?(binary_uri)
+        Fedora6::Client::Binary.update_binary(@config, @binary_uri, filename,
+                                              binary_data, transaction_uri: transaction_uri)
+      else
+        Fedora6::Client::Binary.create_binary(@config, @parent_uri, @binary_identifier,
+                                              filename, binary_data, transaction_uri: transaction_uri)
       end
+    end
 
-      def save(binary_data, filename, transaction_uri: nil)
-        if exists?(binary_uri)
-          Fedora6::Client::Binary.update_binary(@config, @binary_uri, filename,
-                                                binary_data, transaction_uri: transaction_uri)
-        else
-          Fedora6::Client::Binary.create_binary(@config, @parent_uri, @binary_identifier,
-                                                filename, binary_data, transaction_uri: transaction_uri)
-        end
+    def save_by_reference(filename, file_path, transaction_uri: nil)
+      if exists?(binary_uri)
+        Fedora6::Client::Binary.update_binary_by_reference(@config, @binary_uri, filename,
+                                                            file_path, transaction_uri: transaction_uri)
+      else
+        Fedora6::Client::Binary.create_binary_by_reference(@config, @parent_uri, binary_identifier,
+                                                            filename, file_path, transaction_uri: transaction_uri)
       end
+    end
 
-      def save_by_reference(filename, file_path, transaction_uri: nil)
-        if exists?(binary_uri)
-          Fedora6::Client::Binary.update_binary_by_reference(@config, @binary_uri, filename,
-                                                             file_path, transaction_uri: transaction_uri)
-        else
-          Fedora6::Client::Binary.create_binary_by_reference(@config, @parent_uri, binary_identifier,
-                                                             filename, file_path, transaction_uri: transaction_uri)
-        end
+    # Class methods
+
+    def self.get_binary_metadata(config, binary_uri)
+      url = URI.parse("#{binary_uri}/fcr:metadata")
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Get.new url
+        req.basic_auth(config[:user], config[:password])
+        http.request(req)
       end
+    end
 
-      # Class methods
-
-      def self.get_binary_metadata(config, binary_uri)
-        url = URI.parse("#{binary_uri}/fcr:metadata")
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Get.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+    def self.get_binary_versions(config, binary_uri)
+      url = URI.parse("#{binary_uri}/fcr:versions")
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Get.new url
+        req.basic_auth(config[:user], config[:password])
+        http.request(req)
       end
+    end
 
-      def self.get_binary_versions(config, binary_uri)
-        url = URI.parse("#{binary_uri}/fcr:versions")
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Get.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+    def self.create_binary(config, parent_uri, file_identifier, filename, binary_data, transaction_uri: nil)
+      # upload a file by sending a data binary
+      url = URI.parse(parent_uri)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Post.new url
+        req.basic_auth(config[:user], config[:password])
+        req["Atomic-ID"] = transaction_uri if transaction_uri
+        req["Slug"] = file_identifier
+        req["Content-Disposition"] = "attachment; filename=\"#{filename}\""
+        req.body = binary_data
+        http.request(req)
       end
+    end
 
-      def self.create_binary(config, parent_uri, file_identifier, filename, binary_data, transaction_uri: nil)
-        # upload a file by sending a data binary
-        url = URI.parse(parent_uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Post.new url
-          req.basic_auth(config[:user], config[:password])
-          req["Atomic-ID"] = transaction_uri if transaction_uri
-          req["Slug"] = file_identifier
-          req["Content-Disposition"] = "attachment; filename=\"#{filename}\""
-          req.body = binary_data
-          http.request(req)
-        end
+    def self.update_binary(config, binary_uri, filename, binary_data, transaction_uri: nil)
+      # update a file by sending a data binary
+      url = URI.parse(binary_uri)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Put.new url
+        req.basic_auth(config[:user], config[:password])
+        req["Atomic-ID"] = transaction_uri if transaction_uri
+        req["Content-Disposition"] = "attachment; filename=\"#{filename}\""
+        req.body = binary_data
+        http.request(req)
       end
+    end
 
-      def self.update_binary(config, binary_uri, filename, binary_data, transaction_uri: nil)
-        # update a file by sending a data binary
-        url = URI.parse(binary_uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Put.new url
-          req.basic_auth(config[:user], config[:password])
-          req["Atomic-ID"] = transaction_uri if transaction_uri
-          req["Content-Disposition"] = "attachment; filename=\"#{filename}\""
-          req.body = binary_data
-          http.request(req)
-        end
+    def self.create_binary_by_refrerence(config, parent_uri, file_identifier, file_path, transaction_uri: nil)
+      # upload a file by sending a data binary by reference
+      link = "file://#{file_path}; rel=\"http://fedora.info/definitions/fcrepo#ExternalContent\"; handling=\"copy\";"
+      url = URI.parse(parent_uri)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Post.new url
+        req.basic_auth(config[:user], config[:password])
+        req["Atomic-ID"] = transaction_uri if transaction_uri
+        req["Slug"] = file_identifier
+        req["Link"] = link
+        http.request(req)
       end
+    end
 
-      def self.create_binary_by_refrerence(config, parent_uri, file_identifier, file_path, transaction_uri: nil)
-        # upload a file by sending a data binary by reference
-        link = "file://#{file_path}; rel=\"http://fedora.info/definitions/fcrepo#ExternalContent\"; handling=\"copy\";"
-        url = URI.parse(parent_uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Post.new url
-          req.basic_auth(config[:user], config[:password])
-          req["Atomic-ID"] = transaction_uri if transaction_uri
-          req["Slug"] = file_identifier
-          req["Link"] = link
-          http.request(req)
-        end
-      end
-
-      def self.update_binary_by_refrerence(config, binary_uri, file_identifier, file_path, transaction_uri: nil)
-        # update a file by sending a data binary by reference
-        link = "file://#{file_path}; rel=\"http://fedora.info/definitions/fcrepo#ExternalContent\"; handling=\"copy\";"
-        url = URI.parse(binary_uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Put.new url
-          req.basic_auth(config[:user], config[:password])
-          req["Atomic-ID"] = transaction_uri if transaction_uri
-          req["Slug"] = file_identifier
-          req["Link"] = link
-          http.request(req)
-        end
+    def self.update_binary_by_refrerence(config, binary_uri, file_identifier, file_path, transaction_uri: nil)
+      # update a file by sending a data binary by reference
+      link = "file://#{file_path}; rel=\"http://fedora.info/definitions/fcrepo#ExternalContent\"; handling=\"copy\";"
+      url = URI.parse(binary_uri)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Put.new url
+        req.basic_auth(config[:user], config[:password])
+        req["Atomic-ID"] = transaction_uri if transaction_uri
+        req["Slug"] = file_identifier
+        req["Link"] = link
+        http.request(req)
       end
     end
   end
--- lib/fedora6/client/container.rb
+++ lib/fedora6/client/container.rb
@@ -1,57 +1,55 @@
 # frozen_string_literal: true
 
 module Fedora6
-  module Client
-    # Fedora6::Client::Container
-    # Class for managing Fedora6 RDF Container
-    class Container < Client
-      ARCHIVAL_GROUP = "<http://fedora.info/definitions/v4/repository#ArchivalGroup>;rel=\"type\""
-      attr_reader :config, :identifier, :uri
+  # Fedora6::Client::Container
+  # Class for managing Fedora6 RDF Container
+  class Client::Container < Client
+    ARCHIVAL_GROUP = "<http://fedora.info/definitions/v4/repository#ArchivalGroup>;rel=\"type\""
+    attr_reader :config, :identifier, :uri
 
-      def initialize(config = nil, identifier = nil)
-        super
-        @identifier = identifier
-        @uri = "#{self.config["base"]}/#{identifier}"
+    def initialize(config = nil, identifier = nil)
+      super
+      @identifier = identifier
+      @uri = "#{self.config["base"]}/#{identifier}"
+    end
+
+    def save(archival_group: false, transaction_uri: false)
+      response = Fedora6::Client::Container.create_container(
+        @config, @identifier, archival_group, transaction_uri: transaction_uri
+      )
+      validate_response(response)
+      # Return new URI
+      true
+    end
+
+    def metadata
+      response = Fedora6::Client::Container.get_container(@config, @uri)
+      validate_response(response)
+      response.body
+    end
+
+    # Class methods
+
+    def self.get_container(config, uri)
+      url = URI.parse(uri)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Get.new url
+        req.basic_auth(config[:user], config[:password])
+        http.request(req)
       end
+    end
 
-      def save(archival_group: false, transaction_uri: false)
-        response = Fedora6::Client::Container.create_container(
-          @config, @identifier, archival_group, transaction_uri: transaction_uri
-        )
-        validate_response(response)
-        # Return new URI
-        true
-      end
-
-      def metadata
-        response = Fedora6::Client::Container.get_container(@config, @uri)
-        validate_response(response)
-        response.body
-      end
-
-      # Class methods
-
-      def self.get_container(config, uri)
-        url = URI.parse(uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Get.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
-      end
-
-      def self.create_container(config, identifier, archival_group, transaction_uri: false)
-        # create OCFL object
-        url = URI.parse((config[:base]).to_s)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Post.new url
-          req.basic_auth(config[:user], config[:password])
-          req["Atomic-ID"] = transaction_uri if transaction_uri.present?
-          req["Slug"] = identifier if indentifier.present?
-          req["Link"] = ARCHIVAL_GROUP if archival_group
-          req.content_type = "text/turtle"
-          http.request(req)
-        end
+    def self.create_container(config, identifier, archival_group, transaction_uri: false)
+      # create OCFL object
+      url = URI.parse((config[:base]).to_s)
+      Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
+        req = Net::HTTP::Post.new url
+        req.basic_auth(config[:user], config[:password])
+        req["Atomic-ID"] = transaction_uri if transaction_uri.present?
+        req["Slug"] = identifier if indentifier.present?
+        req["Link"] = ARCHIVAL_GROUP if archival_group
+        req.content_type = "text/turtle"
+        http.request(req)
       end
     end
   end
