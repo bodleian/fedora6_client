4f6fb60c0572de2c16648ba1c5c4b40bee2fb029
Tom Wrobel <thomas.wrobel@bodleian.ox.ac.uk>
2023-05-04 21:55:12 +0100

    With versions

---
+++ lib/fedora6/client/version.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Fedora6
+    class Client
+      # Fedora6::Client::Version
+      # Class for managing Fedora6 RDF Version
+      class Version < Client
+        attr_reader :config, :identifier, :uri
+  
+        def initialize(config = nil, parent_uri = nil, identifier = nil)
+          @config = Fedora6::Client::Config.new(config).config
+          @identifier = identifier
+          @uri = "#{parent_uri}}/fcr:versions/#{identifier}"
+        end
+  
+  
+        def metadata
+          response = Fedora6::Client::Version.get(@config, @uri)
+          validate_response(response)
+          response.body
+        end
+  
+        # Class methods
+
+        # Add methods to create new versions
+  
+      end
+    end
+  end
+  
