c22d1caa88cfdb1f46e8b24dabdb5f11c4341de1
kapill65chhpatel <kapil@cottagelabs.com>
2023-06-23 10:39:31 +0530

    refactor some code

--- lib/fedora6/client/transaction.rb
+++ lib/fedora6/client/transaction.rb
@@ -4,86 +4,53 @@ module Fedora6
   class Client
     # Fedora6::Client::Transaction
     class Transaction < Client
-      attr_reader :uri
+      attr_reader :uri, :config
 
       def initialize(config = nil)
-        ## Returns tx_id: the transaction uri
         @config = Fedora6::Client::Config.new(config).config
-        response = Client::Transaction.start_transaction(@config)
+        response = Transaction.start_transaction(@config)
         validate_response(response)
         @uri = response["Location"]
       end
 
-      def get
-        response = Client::Transaction.get_transaction(@config, @uri)
-        validate_response(response, @uri, @config)
-        true
+      { get: 'get_transaction',
+        keep_alive: 'keep_transaction_alive',
+        commit: 'commit_transaction',
+        rollback: 'rollback_transaction'
+      }.each do |key, value|
+        define_method(key) do
+          response = Transaction.send(value, config, uri)
+          validate_response(response, uri, config)
+          true
+        end
       end
-
-      def keep_alive
-        response = Client::Transaction.keep_transaction_alive(@config, @uri)
-        validate_response(response, @uri, @config)
-        true
-      end
-
-      def commit
-        response = Client::Transaction.commit_transaction(@config, @uri)
-        validate_response(response, @uri, @config)
-        true
-      end
-
-      def rollback
-        response = Client::Transaction.rollback_transaction(@config, @uri)
-        validate_response(response, @uri, @config)
-        true
-      end
-
+ 
       # Class methods
 
       def self.start_transaction(config)
-        # Returns a transaction ID
-        url = URI.parse("#{config[:base]}/fcr:tx")
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Post.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+        perform_request(config, "#{config[:base]}/fcr:tx", 'Post')
       end
 
       def self.get_transaction(config, uri)
-        # Returns a transaction ID
-        url = URI.parse(uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Get.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+        perform_request(config, uri, 'Get')
       end
 
       def self.keep_transaction_alive(config, uri)
-        # keeps a transaction that's > 3 minutes long alive
-        url = URI.parse(uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Post.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+        perform_request(config, uri, 'Post')
       end
 
       def self.commit_transaction(config, uri)
-        # keeps a transaction that's > 3 minutes long alive
-        url = URI.parse(uri)
-        Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Put.new url
-          req.basic_auth(config[:user], config[:password])
-          http.request(req)
-        end
+        perform_request(config, uri, 'Put')
       end
 
       def self.rollback_transaction(config, uri)
+        perform_request(config, uri, 'Delete')
+      end
+
+      def self.perform_request(config, uri, request_type)
         url = URI.parse(uri)
         Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == "https") do |http|
-          req = Net::HTTP::Delete.new url
+          req = Object.const_get("Net::HTTP::#{request_type}").new(url)
           req.basic_auth(config[:user], config[:password])
           http.request(req)
         end
